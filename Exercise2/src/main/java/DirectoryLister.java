b45719cc2080f3b378a24cdd0c468f1cec6892c6
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-15 19:55:29 +0200

    core: init

---
+++ Exercise2/src/main/java/DirectoryLister.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DirectoryLister {
+
+    public static List<String> sortList(Path directoryPath) throws IOException {
+
+        List<String> contents = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directoryPath)) {
+
+
+            for (Path entry : stream) {
+                contents.add(entry.toString());
+            }
+
+            Collections.sort(contents);
+
+        } catch (IOException e) {
+
+            throw e;
+        }
+
+        return contents;
+    }
+
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Usage: java DirectoryLister <directory_path>");
+            return;
+        }
+        Path directoryPath = Paths.get(args[0]);
+
+        if (!Files.exists(directoryPath)) {
+            System.err.println("Error: The file does not exist in the path" + directoryPath);
+            return;
+        }
+
+        if (!Files.isDirectory(directoryPath)) {
+            System.err.println("Error: The path is not a directory: " + directoryPath);
+            return;
+
+
+        }
+        try {
+            List<String> sortedContents = sortList(directoryPath);
+
+            for(String e:sortedContents){
+                System.out.println(e);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading directory: " + e.getMessage());
+        }
+    }
+}
+
+
+
