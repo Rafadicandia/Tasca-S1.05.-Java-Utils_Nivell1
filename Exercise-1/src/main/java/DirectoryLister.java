40c301e1f99b1bc480fdb5dc34329e53d57c69cc
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-15 11:42:23 +0200

    refactor: adding validation for the file reading
    we use Path instance to manage files

--- Exercise-1/src/main/java/DirectoryLister.java
+++ Exercise-1/src/main/java/DirectoryLister.java
@@ -1,6 +1,20 @@
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DirectoryLister {
+
+    public static void sortList( Path filePath){
+
+        List<String> contents = new ArrayList<>();
+
+
+
+    }
+
 
     public static void main(String[] args) {
 
@@ -8,22 +22,20 @@ public class DirectoryLister {
             System.out.println("Usage: java DirectoryLister <directory_path>");
             return;
         }
-
-        String directoryPath = args[0];
-
-
-        File directory = new File(directoryPath);
+        Path directoryPath = Paths.get(args[0]);
 
 
-        if (!directory.exists()) {
-            System.err.println("Error: The path does not exist: " + directoryPath);
+
+        if (!Files.exists(directoryPath)) {
+            System.err.println("Error: The file does not exist in the path" + directoryPath);
             return;
         }
 
-        if (!directory.isDirectory()) {
-            System.err.println("Error: The path is not a directory: " + directoryPath);
-            return;
-        }
+        if (!Files.isDirectory(directoryPath) ) {
+        System.err.println("Error: The path is not a directory: " + directoryPath);
+        return;
+    }
+
 
 
     }
