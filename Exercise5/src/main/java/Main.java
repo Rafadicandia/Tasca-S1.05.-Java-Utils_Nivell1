a94d733b999f18fa470afb8855aeafe8fa75a7cd
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-17 13:39:20 +0200

    refactor: change main to Main class

---
+++ Exercise5/src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Usage: java PrintDirectoryTree <directory_path>");
+            return;
+        }
+
+        Path directoryPath = Paths.get(args[0]);
+
+
+        if (!Files.exists(directoryPath)) {
+            System.err.println("Error: The path does not exist:" + directoryPath);
+            return;
+        }
+
+        if (!Files.isDirectory(directoryPath)) {
+            System.err.println("Error: The path is not a directory: " + directoryPath);
+            return;
+        }
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("directoryTree.ser");
+
+        } catch (IOException e) {
+            System.err.println("Error reading directory: " + e.getMessage());
+        }
+    }
+}
--- Exercise5/src/main/java/PrintDirectoryTree.java
+++ Exercise5/src/main/java/PrintDirectoryTree.java
@@ -7,18 +7,18 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.io.Serializable<>
+import java.io.*;
 
 public class PrintDirectoryTree {
 
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
 
     public static void PrintDirectoryTree(Path directoryPath, int depth, PrintWriter writer) throws IOException {
 
         List<Path> contents = new ArrayList<>();
 
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(directoryPath)) {
-
 
             for (Path entry : stream) {
                 contents.add(entry);
@@ -32,7 +32,6 @@ public class PrintDirectoryTree {
 
             throw e;
         }
-
 
         for (Path entry: contents){
 
@@ -52,12 +51,10 @@ public class PrintDirectoryTree {
                     throw e;
                 }
 
-
             }else{
                 if(entry.getFileName().toString().endsWith(".txt")) {
                     ReadTxtFiles.PrintFileContentInConsole(entry);
                 }
-
 
                 String type = "-F";
                 String indent = " ".repeat(depth);
@@ -68,7 +65,6 @@ public class PrintDirectoryTree {
                 writer.println(indent+
                         fileName+type+" "+
                         dateStr
-
                 );
 
             }
@@ -78,32 +74,5 @@ public class PrintDirectoryTree {
     }
 
 
-    public static void main(String[] args) {
 
-        if (args.length != 1) {
-            System.out.println("Usage: java PrintDirectoryTree <directory_path>");
-            return;
-        }
-
-       Path directoryPath = Paths.get(args[0]);
-
-
-        if (!Files.exists(directoryPath)) {
-            System.err.println("Error: The path does not exist:" + directoryPath);
-            return;
-        }
-
-        if (!Files.isDirectory(directoryPath)) {
-            System.err.println("Error: The path is not a directory: " + directoryPath);
-            return;
-        }
-        try {
-            PrintWriter writer = new PrintWriter("ListedDirectoryTree.txt", "UTF-8");
-            PrintDirectoryTree(directoryPath, 0, writer);
-            writer.close();
-
-        } catch (IOException e) {
-            System.err.println("Error reading directory: " + e.getMessage());
-        }
-    }
 }
